e57bf8f5de977b3d64e5dd3222b453a228e7bf85
os8052 <os8052@g.rit.edu>
2026-04-26 16:45:45 +0400

    Fix DriverService return type

--- src/main/java/com/group/f1stats/service/DriverService.java
+++ src/main/java/com/group/f1stats/service/DriverService.java
@@ -7,7 +7,6 @@ import com.group.f1stats.repository.DriverRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class DriverService {
@@ -15,12 +14,12 @@ public class DriverService {
     @Autowired
     private DriverRepository driverRepository;
 
-    public List<Driver> getAllDrivers() {
+   public List<Driver> getAllDrivers() {
         return driverRepository.findAll();
     }
 
-    public Optional<Driver> getDriverById(int id) {
-        return driverRepository.findById(id);
+    public Driver getDriverById(int id) {
+        return driverRepository.findById(id).orElse(null);
     }
 
     public List<Driver> getDriversByNationality(String nationality) {
