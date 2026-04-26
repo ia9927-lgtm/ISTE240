f0dfbe7d5bbb1f6552b75e9bfbec1f4e0f139dda
an6953@g.rit.edu <an6953@rit.edu>
2026-04-26 18:16:41 +0500

    fixed RaceDAO, created RaceController

---
+++ src/main/java/com/group/f1stats/controller/ResultController.java
@@ -0,0 +1,50 @@
+package com.group.f1stats.controller;
+
+
+import com.group.f1stats.model.Result;
+import com.group.f1stats.service.ResultService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/results")
+public class ResultController {
+
+    private final ResultService resultService;
+    public ResultController(ResultService resultService){
+        this.resultService = resultService;
+    }
+
+    @GetMapping("/{id}")
+    public Result findById(@PathVariable Long id){
+        return resultService.findById(id);
+    }
+
+    @GetMapping
+    public List<Result> findAll(){
+        return resultService.findAll();
+    }
+
+    @GetMapping("/search")
+    public List<Result> findByDriverId(@RequestParam Long driverId){
+        return resultService.findByDriverId(driverId);
+    }
+
+    @PostMapping
+    public Result createResult(@RequestBody Result result){
+        return resultService.createResult(result);
+    }
+
+
+    @PutMapping("/{id}")
+    public Result updateResult(@PathVariable Long id, @RequestBody Result result){
+        return resultService.updateResult(id, result);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteResult(@PathVariable Long id){
+        resultService.deleteById(id);
+    }
+}
