45789043d7ffa0553f9ff63e2a6d6da7ba5d7f53
os8052 <os8052@g.rit.edu>
2026-04-26 14:16:51 +0400

    Add data.sql for driver seeding

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT IGNORE INTO drivers (first_name, last_name, nationality, driver_number, team_id)
+VALUES
+('Max', 'Verstappen', 'Dutch', 1, 1),
+('Charles', 'Leclerc', 'Monegasque', 16, 2),
+('Lewis', 'Hamilton', 'British', 44, 3),
+('Lando', 'Norris', 'British', 4, 4),
+('Fernando', 'Alonso', 'Spanish', 14, 5);
